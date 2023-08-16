cebca925960ce3f3af80b048bd2df526e7c4dfdf
Rishi <48538864+DevRishii@users.noreply.github.com>
2023-08-16 00:05:03 -0400

    created main file, added initial code

--- steamWSGameInfoScraper.py
+++ steamWSGameInfoScraper.py
@@ -1,2 +1,10 @@
+#desc: This script will scrape the steam store for game information
+import selenium
+from selenium import webdriver
 
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
 
+driver.get("https://steamcommunity.com/workshop/")
+appRows = driver.find_element(By.CLASS_NAME, "workshopAppsRow")
+print(appRows)
